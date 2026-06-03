01b569051d19518ec8b489eba9baf153363cb57a
Eric Thalakottur <184715709+ericthalakottur@users.noreply.github.com>
2026-06-03 23:07:26 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write, stdin};
+use std::{fs, os::unix::fs::MetadataExt, path::Path};
 
 fn main() {
     let BUILTIN_COMMANDS: Vec<&str> = vec!["echo", "exit", "type"];
@@ -20,6 +21,8 @@ fn main() {
                 } else if input_list[0] == "type" {
                     if BUILTIN_COMMANDS.contains(&input_list[1]) {
                         println!("{} is a shell builtin", input_list[1]);
+                    } else if present_in_path(input_list[1]) != "" {
+                        println!("{} is {}", input_list[1], present_in_path(input_list[1]));
                     } else {
                         println!("{}: not found", input_list[1]);
                     }
@@ -32,4 +35,18 @@ fn main() {
             }
         };
     }
+}
+
+fn present_in_path(command: &str) -> String {
+    let paths = std::env::var("PATH").unwrap();
+    for path in paths.split(":") {
+        let current_path = Path::new(path).join(command);
+        if !current_path.is_file() {
+            continue;
+        }
+        if fs::metadata(&current_path).unwrap().mode() & 0o001 == 1 {
+            return String::from(current_path.to_str().unwrap());
+        }
+    }
+    "".to_string()
 }
