d7eedda631e7664562ba4d03a45c79c3e7943efe
Eric Thalakottur <184715709+ericthalakottur@users.noreply.github.com>
2026-06-04 22:23:37 +0530

    Refactor code to use enums

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,44 @@
 use std::io::{self, Write, stdin};
 use std::{fs, os::unix::fs::MetadataExt, path::Path, process::Command};
 
-fn main() {
-    let BUILTIN_COMMANDS: Vec<&str> = vec!["echo", "exit", "type"];
-    loop {
-        let mut input = String::new();
-        print!("$ ");
-        io::stdout().flush().unwrap();
+enum CommandType {
+    Builtin,
+    Executable,
+    NotExecutable,
+}
 
-        match stdin().read_line(&mut input) {
-            Ok(_) => {
-                if input.trim() == "exit" {
-                    break;
-                }
-                let input_list: Vec<&str> = input.trim().split(" ").collect();
-                if input_list[0] == "echo" {
-                    let output = input_list[1..].join(" ");
-                    println!("{}", output);
-                } else if input_list[0] == "type" {
-                    if BUILTIN_COMMANDS.contains(&input_list[1]) {
-                        println!("{} is a shell builtin", input_list[1]);
-                    } else if is_executable_file(input_list[1]) != "" {
-                        println!("{} is {}", input_list[1], is_executable_file(input_list[1]));
-                    } else {
-                        println!("{}: not found", input_list[1]);
-                    }
-                } else if is_executable_file(&input_list[0]) != "" {
-                    let output = Command::new(input_list[0])
-                        .args(&input_list[1..])
-                        .output()
-                        .expect("failed to execute command");
-                    print!("{}", String::from_utf8_lossy(&output.stdout));
-                } else {
-                    println!("{}: command not found", input.trim());
-                }
-                io::stdout().flush().unwrap();
-            }
-            Err(e) => {
-                eprintln!("{:?}", e);
-            }
-        };
+enum ShellCommand {
+    Exit,
+    Echo(String),
+    Type(CommandType, String),
+    ExternalCommand(String, Vec<String>),
+    InvalidCommand(String),
+}
+
+fn parse_input(input: &str) -> ShellCommand {
+    let builtin_commands: Vec<&str> = vec!["echo", "exit", "type"];
+    if input.trim() == "exit" {
+        return ShellCommand::Exit;
     }
+    let input_list: Vec<&str> = input.trim().split(" ").collect();
+    if input_list[0] == "echo" {
+        return ShellCommand::Echo(input_list[1..].join(" ").to_string());
+    } else if input_list[0] == "type" {
+        let command_type: CommandType = if builtin_commands.contains(&input_list[1]) {
+            CommandType::Builtin
+        } else if is_executable_file(input_list[1]) != "" {
+            CommandType::Executable
+        } else {
+            CommandType::NotExecutable
+        };
+        return ShellCommand::Type(command_type, input_list[1].to_string());
+    } else if is_executable_file(&input_list[0]) != "" {
+        return ShellCommand::ExternalCommand(
+            input_list[0].to_string(),
+            input_list[1..].iter().map(|x| x.to_string()).collect(),
+        );
+    }
+    ShellCommand::InvalidCommand(input.trim().to_string())
 }
 
 fn is_executable_file(command: &str) -> String {
@@ -56,4 +54,49 @@ fn is_executable_file(command: &str) -> String {
         }
     }
     "".to_string()
+}
+
+fn main() {
+    loop {
+        let mut input = String::new();
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        match stdin().read_line(&mut input) {
+            Ok(_) => {
+                let cmd: ShellCommand = parse_input(&input);
+                match cmd {
+                    ShellCommand::Exit => {
+                        break;
+                    }
+                    ShellCommand::Echo(text) => println!("{}", text),
+                    ShellCommand::Type(cmd_type, cmd) => match cmd_type {
+                        CommandType::Builtin => {
+                            println!("{} is a shell builtin", cmd)
+                        }
+                        CommandType::Executable => {
+                            println!("{} is {}", cmd, is_executable_file(&cmd))
+                        }
+                        CommandType::NotExecutable => {
+                            println!("{}: not found", cmd);
+                        }
+                    },
+                    ShellCommand::ExternalCommand(cmd, args) => {
+                        let output = Command::new(cmd)
+                            .args(args)
+                            .output()
+                            .expect("failed to execute command");
+                        print!("{}", String::from_utf8_lossy(&output.stdout));
+                    }
+                    ShellCommand::InvalidCommand(cmd) => {
+                        println!("{}: command not found", cmd);
+                    }
+                };
+                io::stdout().flush().unwrap();
+            }
+            Err(e) => {
+                eprintln!("{:?}", e);
+            }
+        };
+    }
 }
